09b75d91b806567e334fbaea24d33d1774ca2667
GDGouravDey <gdgouravdey@gmail.com>
2024-02-27 18:16:58 +0530

    Bug Fix for Responsiveness

--- fire-app/src/components/Notif.jsx
+++ fire-app/src/components/Notif.jsx
@@ -10,7 +10,7 @@ const Notif = () => {
         <div className="flex flex-row justify-between items-center w-full">
           <div className="flex-1 flex-col xl:px-0 sm:px-16 px-6">
             <div className="flex flex-row justify-between items-center w-full">
-              <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] sm:mt-[-50px] md:mt-[-100px] lg:mt-[-270px] mb-[10px] xl:mt-[-60px]">
+              <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[45px] text-white ss:leading-[100.8px] leading-[75px] sm:mt-[-50px] md:mt-[-100px] lg:mt-[-270px] mb-[10px] xl:mt-[-60px]">
                 <span className="text-gradient">Notifications</span>{" "}
               </h1>
             </div>
